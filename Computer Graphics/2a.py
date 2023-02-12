d654a6f0fe178adb9d042c3f5e918d012c0883fb
Adarsh Gupta <adgupta183@gmail.com>
2023-02-12 21:37:47 +0530

    Update 2a.py

--- Computer Graphics/2a.py
+++ Computer Graphics/2a.py
@@ -1,5 +1,9 @@
 import turtle as t
-import random
+
+# Set up the turtle
+t.speed(10)
+
+# Divide the screen into four regions
 t.forward(900)
 t.back(1800)
 t.home()
@@ -8,14 +12,58 @@ t.forward(400)
 t.back(800)
 t.home()
 
-#t.penup()
-x=random.randrange(10,201)
-y=random.randrange(10,201)
-t.goto(x,y)
-
-#t.penup()
+# Draw a circle in the top left region
+t.penup()
+t.goto(-150, 150)
+t.pendown()
 t.circle(50)
-t.write(x,font=("arial",12,"normal"))
-t.forward(40)
-t.write(y,font=("arial",12,"normal"))
-t.forward(40)
+t.penup()
+t.goto(-150, 90)
+t.pendown()
+t.write("Circle", align="center", font=("Arial", 16, "normal"))
+
+# Draw a rectangle in the top right region
+t.penup()
+t.goto(50, 150)
+t.pendown()
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.penup()
+t.goto(100, 90)
+t.pendown()
+t.write("Rectangle", align="center", font=("Arial", 16, "normal"))
+
+# Draw an eclipse in the bottom left region
+t.penup()
+t.goto(-200, -150)
+t.pendown()
+t.seth(-45)
+for i in range(2):
+    t.circle(100,90)
+    t.circle(100//2,90)
+t.penup()
+t.goto(-150, -200)
+t.pendown()
+t.write("Ellipse", align="center", font=("Arial", 16, "normal"))
+
+# Draw a half eclipse in the bottom right region
+t.penup()
+t.goto(150, -100)
+t.pendown()
+for i in range(1):
+    t.circle(100,90)
+    t.circle(100//2,90)
+t.seth(-45)
+t.penup()
+t.goto(200, -200)
+t.pendown()
+t.write("Half Ellipse", align="center", font=("Arial", 16, "normal"))
+
+# Hide the turtle and exit on click
+t.ht()
+t.exitonclick()
