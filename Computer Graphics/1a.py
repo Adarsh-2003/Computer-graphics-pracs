8c54db7268b4021744de56bed0745d7b93097ab3
Adarsh Gupta <adgupta183@gmail.com>
2023-02-12 12:15:51 +0530

    Update 1a.py

--- Computer Graphics/1a.py
+++ Computer Graphics/1a.py
@@ -1,7 +1,7 @@
 from graphics import*
 win=GraphWin("Hello",300,300)
 win.setBackground(color_rgb(255,0,0))
-pt=point(100,0)
+pt=Point(150,150)
 pt.draw(win)
 cir=Circle(pt,50)
 cir.setFill(color_rgb(100,25,50))
